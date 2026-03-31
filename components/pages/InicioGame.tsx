05e67f8dcdc86f978773493a4509c9e29278e7f8
CinzadeNeve <ticlaudioalves@gmail.com>
2026-03-31 15:25:26 -0300

    fix: melhoria das redesociais

--- components/pages/InicioGame.tsx
+++ components/pages/InicioGame.tsx
@@ -5,6 +5,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { LinkRedeSociais } from "@/components/ui/Link";
 import { BiLogoGmail } from "react-icons/bi";
+import { FaLinkedinIn } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const InicioGame = () => {
@@ -23,12 +24,12 @@ const InicioGame = () => {
                             </ButtonPrimary>
                         </li>
                         <li>
-                            <ButtonPrimary className="w-full text-center">
+                            <ButtonPrimary disabled={true} className="w-full text-center disabled:opacity-70 disabled:cursor-not-allowed">
                                 Sobre
                             </ButtonPrimary>
                         </li>
                         <li>
-                            <ButtonPrimary onClick={() => setStatus("DOACAO")}  className="w-full text-center">
+                            <ButtonPrimary onClick={() => setStatus("DOACAO")} className="w-full text-center">
                                 Doação
                             </ButtonPrimary>
                         </li>
@@ -41,18 +42,28 @@ const InicioGame = () => {
 
 
             {/** Redes Sociais */}
-            <div className="absolute z-[2] bottom-[2rem] left-0 w-full flex items-center justify-center gap-[1rem] max-md:gap-[.5rem] z-[1]">
-                <LinkRedeSociais href="https://www.instagram.com/craudiow/" className="bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600" >
-                    <FaInstagram className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white" />
-                </LinkRedeSociais>
+            <div className="absolute z-[2] bottom-[2rem] left-0 w-full flex flex-col items-center justify-center gap-[1rem] max-md:gap-[.5rem] z-[1]">
+                <h4 className="font-extrabold text-white text-[20px]">Rede Sociais</h4>
+                <div className="flex items-center justify-center gap-[1rem] max-md:gap-[.5rem]">
 
-                <LinkRedeSociais href="https://github.com/CinzadeNeve/" >
-                    <FaGithub className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white" />
-                </LinkRedeSociais>
 
-                <LinkRedeSociais className="bg-gradient-to-r from-[#EA4335] to-red-600" href="mailto:ticlaudioalves@gmail.com" >
-                    <MdEmail className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white"  />
-                </LinkRedeSociais>
+                    <LinkRedeSociais href="https://www.instagram.com/craudiow/" className="bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600" >
+                        <FaInstagram className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white" />
+                    </LinkRedeSociais>
+
+                    <LinkRedeSociais href="https://github.com/CinzadeNeve/" >
+                        <FaGithub className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white" />
+                    </LinkRedeSociais>
+
+                    <LinkRedeSociais className="bg-gradient-to-br from-[#0A66C2] via-[#004182] to-[#002B5C]" href="https://www.linkedin.com/in/cl%C3%A1udio-alves-0813101a1/" >
+                        <FaLinkedinIn className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white" />
+                    </LinkRedeSociais>
+
+                    <LinkRedeSociais className="bg-gradient-to-r from-[#EA4335] to-red-600" href="mailto:ticlaudioalves@gmail.com" >
+                        <MdEmail className="h-[32px] max-md:h-[16px] w-[32px] max-md:w-[16px] text-white" />
+                    </LinkRedeSociais>
+
+                </div>
             </div>
         </>
     )
